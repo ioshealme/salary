76e1a528bd6892780f86ef1873ce1afd14b79085
ioshealme <40445498+ioshealme@users.noreply.github.com>
2018-06-29 14:34:41 +0800

    Add files via upload

--- kc2.cpp
+++ kc2.cpp
@@ -5,9 +5,18 @@
 #include<string>
 #include<fstream>
 
+void read();
+void write();
+void find();
+void list();
+void mod();
+void del();
+void add();
+float grsds(float m);
+
 int n=0;//记录员工数量，全局变量
 using namespace std;
-struct member{
+ struct member{
 	char name[50];//姓名
 	char ID[50];                // 岗位工资、薪级工资、职务津贴、绩效工资、应发工资、个人所得税、实发工资
 	float gwsalary;
@@ -18,27 +27,95 @@ struct member{
 	float grsalary;
 	float reasalary;
 
-   }member1[50],newmember;
-    
-    member zggz[n];
+};                          //member1[50],newmember;
+struct member zggz[50];// 
 
-   void read()
-	{  FILE *fp;
-		  int i;                                        //ofstream fout1("gx.date",ios::binary); n 为记录员工的数量
-		if ((fp=fopen("gx.date","wb"))==NULL){   //用二进制打开文件gx.date从数据文件中读取职工工资数据、read函数 初始化职工工资结构体数组并统计当前数据文件中职工
-                                                 //人数，
-            printf("cannot build this file\n");
+
+int main()
+{
+	read();
+	printf("职工工资管理系统\n");
+	int n,m;
+	do
+	{
+		printf("请选择：\n");
+		printf("=========================================================");
+		printf("|               1.查询职工工资记录                      |");
+		printf("|               2.修改职工工资记录                      |");
+		printf("|               3.添加职工工资记录                      |");
+		printf("|               4.删除职工工资记录                      |");
+		printf("|               5.保存职工工资记录                      |");
+		printf("|               6.浏览职工记录                          |");
+		printf("|               7.退出系统                              |");
+		printf("=========================================================");
+
+		printf("你的选择是：\n");
+		scanf("%d",&n);
+		if(n<7||n>7)
+			printf("你的输入不正确，请重新输入1-7的数字\n");
+		
+			switch(n)
+			{
+			case 1:find();
+			case 2:mod();
+			case 3:add();
+			case 4:del();
+			case 5:write();
+			case 6:list();
+			case 7:printf("退出系统，欢迎下次光临!\n");
+			default:exit(0);//退出系统，exit(0)函数有退出功能。
+			}
+			
+			
+			
+			
+			
+			return 0;
+
+			
+			
+			
+			
+			
+			
+	}
+
+
+
+
+void read()
+{           //从数据文件中读取职工工资数据、初始化职工工资结构体数组并统计当前数据文件中职工人数，并将职工人数存在全局变量n中，以备其他函数使用。
+	  
+	   FILE *fp;
+		int i;                                  //ofstream fout1("gx.date",ios::binary); n 为记录员工的数量
+		if ((fp=fopen("gx.date","wb"))==NULL){  //用二进制打开文件gx.date从数据文件中读取职工工资数据
+            printf("cannot open this file\n");
 			exit(1);
 		}                                       //并将职工人数存在全局变量n中，以备其他函数使用。
 	
 	
 
-	if(fp)
+		else
 	{  
-		fp=fopen("gx.date","rb");//打开文件
-	    fseek(fp,0,SEEK_END);   //文件指针移到文件尾  
-      for(int i=0;i<n;i++)     //初始化职工信息
+ //en("gx.date","rb");//打开文件
+
+		n=n+1;
+		 fseek(fp,0,SEEK_END);  //文件指针移到文件尾  ?
+		while(fread(&zggz[n],sizeof(zggz[n]),1,fp)==1)
+			n=n+1;
+		 fclose(fp);//关闭指针文件。
+		 printf("职工工资读取完毕，共有%d人！",n);
+		
+	   
+
+	
+
+
+    /*for(int i=0;i<n;i++)     //初始化职工信息
 	  {
+		 
+
+
 		  scanf("%s",&member1[i].name);
 		  scanf("%s",&member1[i].ID);
 		  scanf("%f",&member1[i].gwsalary);
@@ -64,13 +141,12 @@ struct member{
           zggz[n].grsalary=member1[n].grsalary;
 		  zggz[n].reasalary=member1[n].reasalary;
 	  }
-
+	 */
 	
-	fclose(fp);
-   
+	
    }
   
-
+   }
 void write()               //将zggz结构体数组中的记录保存到gz.dat数据文件中。
 {  
 	FILE *fp;
@@ -79,7 +155,7 @@ void write()               //将zggz结构体数组中的记录保存到gz.dat数据文件中。
 		if(n>0)
 	{
 		                                          //ofstream fout1("gx.date",ios::binary);
-		if ((fp=fopen("gx.date","wb+")==NULL){   //用二进制打开文件gx.date从数据文件中读取职工工资数据、read函数 初始化职工工资结构体数组并统计当前数据文件中职工
+		if ((fp=fopen("gz.dat","wb"))==NULL){ //用二进制打开文件gx.date从数据文件中读取职工工资数据、read函数 初始化职工工资结构体数组并统计当前数据文件中职工
                                                  //人数，
             printf("cannot open this file\n");
 			exit(-1);
@@ -88,7 +164,7 @@ void write()               //将zggz结构体数组中的记录保存到gz.dat数据文件中。
 	for(int i=0;i<n;i++)
 	{ 
 		
-	     fwrite(&zggz[i],sizeof(struct member),1,fp);  //写一次，每次长度是member的长度,将n个员工写入文件
+	     fwrite(&zggz[i],sizeof(struct member),n,fp);  //写n次，每次长度是member的长度,将n个员工写入文件gz. 对的
          rewind(fp);//复位置偏移量到初始状态；
 
 		 fclose(fp);
@@ -105,7 +181,7 @@ void find()         //根据工号查询相应职工的工资记录
 	FILE *fp;
 	int i;	
 	char gonghaop[15];
-    if((fp=open("gz,date","rb"))==NULL)
+    if((fp=open("gz.date","rb"))==NULL)
 	{
 		printf("cannot open this file\n");
 			exit(1);
@@ -115,7 +191,15 @@ void find()         //根据工号查询相应职工的工资记录
     printf("请输入要查找的编号：\n");
     scanf("%s",gonghao);
 	for(i=0;fread(&zggz[i],sizeof(struct member),1,fp)!=0;i++)
+		if(!strcmp(zggz[i].ID,gonghao)) break;
+    if(feof(fp))
 	{
+		printf("未找到该员工：\n");
+
+		exit(2);
+
+	}
+	
 		print("员工编号为：%s",zggz[i].ID);
 		printf("员工姓名为：%s",zggz[i].name);  
 		printf("员工岗位工资为：%f",zggz[i].gwsalary);
@@ -128,16 +212,9 @@ void find()         //根据工号查询相应职工的工资记录
 		i=i+1;
 
 		
-	}
+	
 
-	if(!strcmp(zggz[i].ID,gonghao));
-    if(feof(fp))
-	{
-		printf("未找到该员工：\n");
-
-		exit(2);
-
-	}
+	
 
 
 	fclose(fp);
@@ -156,14 +233,14 @@ void list()//
 	FILE *fp;
 	int i;	
 	
-    if((fp=open("gz,date","r"))==NULL)
+    if((fp=open("gz.date","rb")
 	{
 		printf("cannot open this file\n");
 			exit(0);
 	
 	}
 	printf("浏览员工的数据");
-	for(i=0;fread(&zggz[i],sizeof(struct member),1,fp)!==NULL)
+	for(i=0;fread(&zggz[i],sizeof(struct member),1,fp)!==NULL;i++)
 	{
 		    printf("%6d",i+1);
 		    printf("%s",zggz[i].name);
@@ -187,7 +264,7 @@ void list()//
 void modify()                  //指定工号，修改该职工的工资记录，其中要调用grsds()函数计算个人所得税。
 {
 	FILE *fp;
-	int i,j;	
+	int i;
 	char gonghao[15];
 
     if((fp=open("gz,date","rb+"))==NULL)
@@ -197,8 +274,43 @@ void modify()                  //指定工号，修改该职工的工资记录，其中要调用grsds
 	
 	}
 	
-	printf("请输入你要修改的工号")；
-    scanf("%s",gonghao);
+ 	printf("请输入你要修改的工号");
+    scanf("%s",gonghao);//从这开始不一样，另一种的
+	for(i=0;i<=n;i++)
+	{
+		 printf("输出该职工的信息");
+		 printf("%s",zggz[i].name);
+          printf("%s",zggz[i].ID);
+	      printf("%f",zggz[i].gwsalary);
+            printf("%f",zggz[i].ransalary);
+			printf("%f",zggz[i].zwsalary);
+			printf("%f",zggz[i].jxsalary);
+		    printf("%f",zggz[i].yfsalary);
+			printf("%f",zggz[i].grsalary);
+			printf("%f",zggz[i].reasalary);	
+
+			printf("请输入该职工新的信息");
+			 scanf("%s",&zggz[i].name);
+		  scanf("%s",&zggz[i].ID);
+		  scanf("%f",&zggz[i].gwsalary);
+		  scanf("%f",&zggz[i].ransalary);
+          scanf("%f",&zggz[i].zwsalary);
+          scanf("%f",&zggz[i].jxsalary);
+		  scanf("%f",&zggz[i].yfsalary);
+		  scanf("%f",&zggz[i].grsalary);
+		  scanf("%f",&zggz[i].reasalary);
+
+		  zggz[i].yfsalsry=zggz[i].gwsalary+zggz[i].xisalary+zggz[i].zwsalary+zggz[i].jxsalary;
+		  zggz[i].grsds=grsds(zggz[i].yfsalsry);
+		  zggz[i].sfsalary=zggz[i].yasalary+zggz[i].grsds;
+
+		  return;
+
+
+	}
+
+
+	/*
 
 	for(i=0;fread(&zggz[i],sizeof(struct member),1,fp);i++)
 		if(!strcmp(zggz[i].ID,gonghao))break;
@@ -250,6 +362,9 @@ void modify()                  //指定工号，修改该职工的工资记录，其中要调用grsds
 			     printf("%f",zggz[i].grsalary);
 			     printf("%f",zggz[i].reasalary);
 		  }
+		  */
+
+
 		  fclose(fp);
 
 	
@@ -403,14 +518,87 @@ void add()//
 			                                              for(i=y;i>x+1;i--)
 
 		                                                         }*/
-	}
 
+
+
+
+
+
+
+
+float grsds(float m)  //计算个人所得税，按照个人所得税税率表来计算，税率表如表4.1所示。
+
+{
+
+float x;	/*当月应纳税所得额*/
+
+float y=0;	/*当月应纳税额*/
+
+int n;
+
+x=m-2000;
+
+if(x>0)
+
+if(x<500)
+n=1;
+
+else if(x<2000)
+n=2;
+
+else if(x<5000)
+n=3;
+
+else if(x<20000)
+n=4;
+
+else if(x<40000)
+n=5;
+
+else if(x<60000)
+n=6;
+
+else if(x<80000)
+n=7;
+
+else if(x<100000)
+n=8;
+
+else n=9;
+
+switch(n)	/*此处利用case的特点*/
+
+{
+
+case 9:y=(x-100000)*0.45;x=100000;
+
+case 8:y+=(x-80000)*0.40;x=80000;
+
+case 7:y+=(x-60000)*0.35;x=60000;
+
+case 6:y+=(x-40000)*0.30;x=40000;
+
+case 5:y+=(x-20000)*0.25;x=20000;
+
+case 4:y+=(x-5000)*0.20;x=5000;
+
+case 3:y+=(x-2000)*0.15;x=2000;
+
+case 2:y+=(x-500)*0.10;x=500;
+
+case 1:y+=x*0.05;
+
+}
+
+return y;
 
 }
 
 
-int grsds(zggz[],int y)//计算个人所得税，按照个人所得税税率表来计算，税率表如表4.1所示。
-{
+
+
+
+	/*
 	FILE *fp;
 	int n ,num;
 	if((fp=fopen("gz.date","rb"))==NULL)
@@ -440,21 +628,9 @@ int grsds(zggz[],int y)//计算个人所得税，按照个人所得税税率表来计算，税率表如表4
 		{ grsalary=yfsalary*0.45; }
 
 
+*/
 
-
-
-}
-
-
-int main()
-{
-	read();
-	ptintf("职工工资管理系统\n");
-	int n,m;
-	do
-	{
-
-	}
+//fclose(fp);
 
 
 
@@ -472,6 +648,16 @@ int main()
 
 
 
-return 0;
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
